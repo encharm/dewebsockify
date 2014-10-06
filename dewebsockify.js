b82bf903ec69bebaa8291b1e1f41430c3b4e3a17
Damian Kaczmarek <rush@rushbase.net>
2014-10-06 02:15:44 +0200

    Add some basic logging

--- dewebsockify.js
+++ dewebsockify.js
@@ -14,25 +14,37 @@ var server = net.createServer(function(socket) {
   var ws = new WebSocket(process.argv[2], {
     rejectUnauthorized: false
   });
-  ws.on('open', function() {
-    socket.on('data', function(data) {
-      ws.send(data);
-    });
-  });
-  ws.on('message', function(data, flags) {
-    socket.write(data);
-  });
-  socket.on('end', function() {
-    ws.close();
-  });
-  socket.on('error', function() {
+
+  socket.on('error', function(err) {
     ws.close();
     socket.close();
   });
 
+  ws.on('error', function(err) {
+    console.log("Connection from", socket.address().address ,"to end-point...", err.toString());
+    socket.end();
+  });
+
+  ws.on('open', function() {
+    socket.address = socket.address().address;
+    console.log("Connection from", socket.address ,"accepted");
+    socket.on('data', function(data) {
+      ws.send(data);
+    });
+  });
+
+  ws.on('message', function(data, flags) {
+    socket.write(data);
+  });
+
+  socket.on('end', function() {
+    console.log("Connection from", socket.address ,"ended");
+    ws.close();
+  });
+
   ws.on('close', function() {
+    console.log("Web socket server has closed connection, closing socket to", socket.address);
     socket.end();
   });
 
 }).listen(port);
-
